9802a1a9d4a42b2f5766b52eb1039a0e9f8db1db
waniyaemaan-cloud <waniya.emaan@gmail.com>
2026-03-01 19:52:55 +0500

    Add files via upload

---
+++ complexmaths.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+using namespace std;
+main(){
+
+
+cout<<"Number1:";
+int n1;
+cin>> n1;
+cout<<"Number2:";
+int n2;
+cin>> n2;
+cout<<"Number3:";
+int n3;
+cin>> n3;
+cout<<"Number4:";
+int n4;
+cin>> n4;
+cout<<"Number5:";
+int n5;
+cin>> n5;
+cout<<"Number6:";
+int n6;
+cin>> n6;
+cout<<"Number7:";
+int n7;
+cin>> n7;
+cout<<"Number8:";
+int n8;
+cin>> n8;
+cout<<"Number9:";
+int n9;
+cin>> n9;
+cout<<"Number10:";
+int n10;
+cin>> n10;
+cout<<"Number11:";
+int n11;
+cin>>  n11;
+cout<<"Number12:";
+int n12;
+cin>> n12;
+cout<<"Number13:";
+int n13;
+cin>> n13;
+cout<<"Number14:";
+int n14;
+cin>> n14;
+cout<<"Number15:";
+int n15;
+cin>> n15;
+
+int sum;
+sum=n1+n2+n3+n4+n5;
+int product;
+product=n6*n7*n8*n9*n10;
+int subtraction;
+subtraction=n11-n12-n3-n14-n15;
+int finalResult;
+finalResult=(sum+product)-subtraction;
+cout<<"The final result is:"<<finalResult;
+}
