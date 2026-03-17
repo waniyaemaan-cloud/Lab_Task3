0b0bedff147841a96ca170f4cf0aad5a89fd0ece
waniyaemaan-cloud <waniya.emaan@gmail.com>
2026-03-17 22:52:36 +0500

    Add files via upload

--- Problem4.cpp
+++ Problem4.cpp
@@ -1,24 +1,36 @@
 #include<iostream>
 using namespace std;
-main(){
-    cout<<"Enter the Number of Red Roses: ";
-    int Red_Roses;
-    cin>> Red_Roses;
-    cout<<"Enter the Number of White Roses: ";
-    int White_Roses;
-    cin>> White_Roses;
-    cout<<"Enter the Number of Tulips: ";
-    int Tulips;
-    cin>> Tulips;
-    float Total_Price;
-    Total_Price=Red_Roses*2.00+White_Roses*4.10+Tulips*2.50;
-    if(Total_Price>200){
-        float Discount;
-        Discount=Total_Price*0.20;
-    
-    float After_Discount;
-    After_Discount=Total_Price-Discount;
-    cout<<"Original Price:"<<Total_Price<<endl;
-    cout<<"Discounted Price: "<<After_Discount;
-}
+int main()
+{
+    int number_of_number_lines,number,p1=0,p2=0,p3=0,p4=0,p5=0;
+    cout<<"Enter the numbers : ";
+    cin>>number_of_number_lines;
+
+    for(int i=1;i<=number_of_number_lines;i++){
+        cout<<"Enter the no. :";
+        cin>>number;
+
+        if(number>=1 &&number<200){
+            p1++;
+        }
+        else if(number>=200 &&number<400){
+            p2++;
+        }
+        else if(number>=400&&number<600){
+            p3++;
+        }
+         else if(number>=600&&number<800){
+            p4++;
+        }
+         else if(number>=800&&number<=1000){
+            p5++;
+        }
+
+    }
+
+    cout<<"Percentage of P1 : "<<(p1*100.0)/number_of_number_lines<<" %"<<endl;
+    cout<<"Percentage of P2 : "<<(p2*100.0)/number_of_number_lines<<" %"<<endl;
+    cout<<"Percentage of P3 : "<<(p3*100.0)/number_of_number_lines<<" %"<<endl;
+    cout<<"Percentage of P4 : "<<(p4*100.0)/number_of_number_lines<<" %"<<endl;
+    cout<<"Percentage of P5 : "<<(p5*100.0)/number_of_number_lines<<" %"<<endl;
 }
